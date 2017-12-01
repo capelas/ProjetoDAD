458302d4aec0ee1ea1347f975d7eaba7b46c456b
capelas <andrecapelasfilipe@hotmail.com>
2017-12-01 15:20:15 +0000

    projeto inicio

---
+++ JogoDaMemoria-master/laravel_server/routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+        return $request->user();
+});
+
+Route::post('login', 'LoginControllerAPI@login');
+Route::middleware('auth:api')->post('logout',
+ 'LoginControllerAPI@logout');
+
+Route::get('teste', function () {
+ return response()->json(['msg'=>'Só um teste'], 200);
+});
+
+Route::middleware('auth:api')->get('teste', function () {
+ return response()->json(['msg'=>'Só um teste'], 200);
+});
