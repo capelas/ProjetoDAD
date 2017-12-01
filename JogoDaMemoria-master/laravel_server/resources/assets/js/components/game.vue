893734cd958fad5499f0667e07af51b5cb106c83
Leonardo Silva <djdavinci.67mustang@gmail.com>
2017-12-01 16:50:38 +0000

    teste

---
+++ JogoDaMemoria-master/laravel_server/resources/assets/js/components/game.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="gameseparator">
+		<div>
+			<h2 class="text-center">{{game.gameID}}</h2>
+			<br>
+		</div>
+		<div class="game-zone-content">       
+			<div class="board">
+				<div v-for="(piece,key) of game.board">
+					<img v-bind:src="pieceImageURL(piece)">
+				</div>
+			</div>
+			<hr>
+		</div>  
+	</div>
+</template>
+
+<script type="text/javascript">
+	console.log(Hello);
+	export default{
+		props: ['game'],
+		data: function () {
+			return{
+
+			}
+		},
+		methods:{
+			pieceImageURL: function (piece) {
+				var imgSrc = String(piece);
+				return 'img/'+imgSrc+'.png';
+			}
+		}
+	}
+</script>
---
+++ JogoDaMemoria-master/laravel_server/resources/assets/js/vueapp.js
@@ -0,0 +1,39 @@
+/*jshint esversion: 6 */
+
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+import VueRouter from 'vue-router';
+//import VueSocketio from 'vue-socket.io';
+
+Vue.use(VueRouter);
+
+//Vue.use(VueSocketio, 'http://192.168.10.10:8080');
+//Vue.use(VueSocketio, 'http://192.168.10.1:8080');
+
+const game = Vue.component('game',require('./components/game.vue'));
+
+const routes = [
+  { path: '/', redirect: '/game' },
+  { path: '/game', component: game.vue }
+];
+
+const router = new VueRouter({
+  routes:routes
+});
+
+const app = new Vue({
+  router,
+  data:{
+    //player1:undefined,
+    //player2: undefined,
+    //departments: [],
+  }
+}).$mount('#app');
